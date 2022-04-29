f0f1cc006d91e0960d31a9d05e5367a3a0378177
Pol Sañé Jové <pol.sane@uvic.cat>
2022-04-29 18:42:56 +0200

    I have started the prototype of the MultipleLineCommandschemaLoader, it also implements the SchemaLoader interface

---
+++ utilities/src/main/java/cat/uvic/teknos/m06/MangaApp/utilities/SingleLineCommandSchemaLoader.java
@@ -0,0 +1,13 @@
+package cat.uvic.teknos.m06.MangaApp.utilities;
+
+public class SingleLineCommandSchemaLoader implements SchemaLoader{
+    private final String schemaPath;
+    public SingleLineCommandSchemaLoader(String schemaPath){
+        this.schemaPath=schemaPath;
+    }
+
+    @Override
+    public void load() {
+                
+    }
+}
--- utilities/src/main/java/cat/uvic/teknos/m06/MangaApp/utilities/exceptions/SchemaLoaderException.java
+++ utilities/src/main/java/cat/uvic/teknos/m06/MangaApp/utilities/exceptions/SchemaLoaderException.java
@@ -1,7 +1,24 @@
 package cat.uvic.teknos.m06.MangaApp.utilities.exceptions;
 
 public class SchemaLoaderException extends RuntimeException{
+    public SchemaLoaderException() {
+    }
 
+    public SchemaLoaderException(String message) {
+        super(message);
+    }
+
+    public SchemaLoaderException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public SchemaLoaderException(Throwable cause) {
+        super(cause);
+    }
+
+    public SchemaLoaderException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
 
 
