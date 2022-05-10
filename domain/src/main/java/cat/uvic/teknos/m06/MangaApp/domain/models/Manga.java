0d7203e04b7676d0b3024beaa93056d6478d35c0
Pol Sañé Jové <pol.sane@uvic.cat>
2022-05-10 20:45:20 +0200

    I have added the module of the manga table

--- domain/src/main/java/cat/uvic/teknos/m06/MangaApp/domain/models/Manga.java
+++ domain/src/main/java/cat/uvic/teknos/m06/MangaApp/domain/models/Manga.java
@@ -1,5 +1,43 @@
 package cat.uvic.teknos.m06.MangaApp.domain.models;
 
 public class Manga {
-    
+    private int mangaId;
+    private String title;
+    private String description;
+    private int coverId;
+    public int getMangaId() {
+        return mangaId;
+    }
+
+    public void setMangaId(int mangaId) {
+        this.mangaId = mangaId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCoverId() {
+        return coverId;
+    }
+
+    public void setCoverId(Cover c) {
+        coverId=c.getCoverId();
+    }
+
+
+
+
 }
