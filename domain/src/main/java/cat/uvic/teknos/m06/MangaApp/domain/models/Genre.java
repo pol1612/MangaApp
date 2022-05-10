66f3be8eaf1c35effbd51e95aea67a83b7b4e186
Pol Sañé Jové <pol.sane@uvic.cat>
2022-05-10 20:47:52 +0200

    I have added the module of the genre table

---
+++ domain/src/main/java/cat/uvic/teknos/m06/MangaApp/domain/models/Genre.java
@@ -0,0 +1,33 @@
+package cat.uvic.teknos.m06.MangaApp.domain.models;
+
+public class Genre {
+    private int genreId;
+    private String name;
+
+    public int getGenreId() {
+        return genreId;
+    }
+
+    public void setGenreId(int genreId) {
+        this.genreId = genreId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private String description;
+
+}
