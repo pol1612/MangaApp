e7e9c9e1de5b7261f574cc1a45446fc0789f4fe9
unknown <pol.sane@uvic.com>
2022-05-15 15:41:28 +0200

    I have modified the signature of the methods of the RepositoriesDo Interface, they now recieve no parameters, the connection and module will be given to the constructor of each repository

--- domain/src/main/java/cat/uvic/teknos/m06/MangaApp/domain/repositories/RepositoriesDo.java
+++ domain/src/main/java/cat/uvic/teknos/m06/MangaApp/domain/repositories/RepositoriesDo.java
@@ -1,8 +1,8 @@
 package cat.uvic.teknos.m06.MangaApp.domain.repositories;
 
 public interface RepositoriesDo {
-    void save(Object module);
-    void delete(Object module);
-    void GetById(Object module);
-    void GetAll(Object module);
+    void save();
+    void delete();
+    void GetById();
+    void GetAll();
 }
