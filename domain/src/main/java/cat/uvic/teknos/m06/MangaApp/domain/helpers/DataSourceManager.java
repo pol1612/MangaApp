a796f12e99fcfe82c3f44b8c12ee386632e6a079
Pol Sañé Jové <pol.sane@uvic.cat>
2022-05-17 20:41:46 +0200

    I have finsished the DataSourceManager(DSM) class, now i will do a ConnectionManager interface that is implemented by the DSM and in the repositories the reference of the DSM will be a CM reference because if i want to use other managers i will not have to change the repositories classes as long as this manager implements the CM interface

--- domain/src/main/java/cat/uvic/teknos/m06/MangaApp/domain/helpers/DataSourceManager.java
+++ domain/src/main/java/cat/uvic/teknos/m06/MangaApp/domain/helpers/DataSourceManager.java
@@ -1,18 +1,22 @@
 package cat.uvic.teknos.m06.MangaApp.domain.helpers;
+import cat.uvic.teknos.m06.MangaApp.domain.exceptions.DataSourceManagerException;
+
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataSourceManager {
     private ConnectionProperties connectionProperties;
-    DataSourceManager(ConnectionProperties connectionProperties){
+    public DataSourceManager(ConnectionProperties connectionProperties){
        this.connectionProperties=connectionProperties;
-   }
-   public void main(String[] args) {
+    }
+   public Connection getConnection() {
        try (var connection = DriverManager.getConnection(
                connectionProperties.getUrl(), connectionProperties.getUser(), connectionProperties.getPassword())) {
-
+            return connection;
        } catch (SQLException e) {
-
+            throw new DataSourceManagerException(e);
        }
    }
+
 }
