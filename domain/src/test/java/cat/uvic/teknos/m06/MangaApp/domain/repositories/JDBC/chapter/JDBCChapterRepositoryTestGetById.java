8a10ecf35f381742d8f195fb43deb2736755617e
unknown <pol.sane@uvic.com>
2022-05-23 23:50:05 +0200

    I have tested the GetById method of JDBCChapterRepository and it works

---
+++ domain/src/test/java/cat/uvic/teknos/m06/MangaApp/domain/repositories/JDBC/chapter/JDBCChapterRepositoryTestGetById.java
@@ -0,0 +1,31 @@
+package cat.uvic.teknos.m06.MangaApp.domain.repositories.JDBC.chapter;
+
+import cat.uvic.teknos.m06.MangaApp.domain.helpers.ConnectionProperties;
+import cat.uvic.teknos.m06.MangaApp.domain.helpers.DataSourceManager;
+import cat.uvic.teknos.m06.MangaApp.domain.modules.Chapter;
+import cat.uvic.teknos.m06.MangaApp.domain.repositories.JDBC.JDBCChapterRepository;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JDBCChapterRepositoryTestGetById {
+
+    @Test
+    void getById() {
+        Chapter chapter;
+        var connectionProperties=new ConnectionProperties();
+        connectionProperties.setUrl("jdbc:mysql://localhost:3306/mysql");
+        connectionProperties.setUser("root");
+        connectionProperties.setPassword(null);
+        DataSourceManager dataSourceManager=new DataSourceManager(connectionProperties);
+        JDBCChapterRepository jdbcChapterRepository=new JDBCChapterRepository(dataSourceManager);
+        chapter=jdbcChapterRepository.GetById(2);
+        System.out.println(chapter.getChapterId());
+        System.out.println(chapter.getChapterNumber());
+        System.out.println(chapter.getChapterTitle());
+        System.out.println(chapter.getMangaId());
+        System.out.println(chapter.getDateOfUploading());
+
+
+    }
+}
