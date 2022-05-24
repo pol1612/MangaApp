3284a503cf10cc4d9870b43c9f5a029f97d57306
unknown <pol.sane@uvic.com>
2022-05-24 06:55:03 +0200

    I have tested the GetAll method of JDBCChapterRepository and it works, now all the Repositories are fully implemented

---
+++ domain/src/test/java/cat/uvic/teknos/m06/MangaApp/domain/repositories/JDBC/chapter/JDBCChapterRepositoryTestGetAll.java
@@ -0,0 +1,29 @@
+package cat.uvic.teknos.m06.MangaApp.domain.repositories.JDBC.chapter;
+
+import cat.uvic.teknos.m06.MangaApp.domain.helpers.ConnectionProperties;
+import cat.uvic.teknos.m06.MangaApp.domain.helpers.DataSourceManager;
+import cat.uvic.teknos.m06.MangaApp.domain.modules.Chapter;
+import cat.uvic.teknos.m06.MangaApp.domain.repositories.JDBC.JDBCChapterRepository;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JDBCChapterRepositoryTestGetAll {
+
+    @Test
+    void getAll() {
+        var connectionProperties=new ConnectionProperties();
+        connectionProperties.setUrl("jdbc:mysql://localhost:3306/mysql");
+        connectionProperties.setUser("root");
+        connectionProperties.setPassword(null);
+        DataSourceManager dataSourceManager=new DataSourceManager(connectionProperties);
+        JDBCChapterRepository jdbcChapterRepository=new JDBCChapterRepository(dataSourceManager);
+        List<Chapter> chapterList=new ArrayList<>();
+        chapterList=jdbcChapterRepository.GetAll();
+        System.out.println(chapterList.get(0).getChapterTitle());
+        System.out.println(chapterList.get(1).getChapterTitle());
+    }
+}
