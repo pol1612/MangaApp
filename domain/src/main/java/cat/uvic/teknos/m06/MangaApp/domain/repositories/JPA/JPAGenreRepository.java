cbf92bf1102b0c108fa717ed047f8c9d23d9ac8b
Pol Sañé Jové <pol.sane@uvic.cat>
2022-05-27 19:24:42 +0200

    I have implemented the save method (the insert feature) of JPAGenreRepository

--- domain/src/main/java/cat/uvic/teknos/m06/MangaApp/domain/repositories/JPA/JPAGenreRepository.java
+++ domain/src/main/java/cat/uvic/teknos/m06/MangaApp/domain/repositories/JPA/JPAGenreRepository.java
@@ -12,10 +12,17 @@ public class JPAGenreRepository implements RepositoriesDo<Genre, Integer> {
         this.entityManagerFactory=entityManagerFactory;
     }
     @Override
-    public void save(Genre model) {
-
+    public void save(Genre genre) {
+        if(genre.getGenreId()==0){
+            insert(genre);
+        }
     }
-
+    private void insert(Genre genre){
+        var entityManager=entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(genre);
+        entityManager.getTransaction().commit();
+    }
     @Override
     public void delete(Integer id) {
 
