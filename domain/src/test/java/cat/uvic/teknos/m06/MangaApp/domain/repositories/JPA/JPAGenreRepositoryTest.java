af92121efbffe33e0813bc6ed7f218607cd90b9f
Pol Sañé Jové <pol.sane@uvic.cat>
2022-05-27 19:47:46 +0200

    after testing the save method and it not working, I have modified a little bud in persistence.xml and now it works :)

--- domain/src/test/java/cat/uvic/teknos/m06/MangaApp/domain/repositories/JPA/JPAGenreRepositoryTest.java
+++ domain/src/test/java/cat/uvic/teknos/m06/MangaApp/domain/repositories/JPA/JPAGenreRepositoryTest.java
@@ -1,5 +1,6 @@
 package cat.uvic.teknos.m06.MangaApp.domain.repositories.JPA;
 
+import cat.uvic.teknos.m06.MangaApp.domain.models.Genre;
 import org.junit.jupiter.api.Test;
 
 import javax.persistence.EntityManagerFactory;
@@ -12,7 +13,14 @@ class JPAGenreRepositoryTest {
     @Test
     void save() {
         var entityManagerFactory= Persistence.createEntityManagerFactory("MangaApp_mysql");
-
+        var jpaGenreRepository=new JPAGenreRepository(entityManagerFactory);
+        var genre=new Genre();
+        genre.setName("Martial2 arts");
+        genre.setDescription("uuuuuuuuuuuuuuuuuuuuuuuuuuu");
+        assertDoesNotThrow(()-> {
+            jpaGenreRepository.save(genre);
+        });
+        assertTrue(genre.getGenreId()>0);
     }
 
     @Test
